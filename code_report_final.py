03d5163ebaa36f5289a38ffc69ba896e56a38ce4
Nguyễn Văn Thơ <nguyenvanthohaui@gmail.com>
2023-09-25 10:45:06 +0700

    1st commit: thiet ke UI

--- code_report_final.py
+++ code_report_final.py
@@ -3,9 +3,34 @@ import numpy as np
 import soundfile as sfile
 from pydub.playback import play
 import time
+from tkinter import *
+from tkinter import messagebox
+
+win = Tk()
+win.title("Sound Recognition")
+win.geometry('600x607')
+win.config(bg = '#9C61CC')
+
+#thiet ke UI
+Label(win, text = "Phần mềm nhận dạng \nchó và mèo", font = ('Times new roman', 15), bg = 'white', fg = 'Black', width = 20, height = 2, relief='solid', borderwidth = 2).place(x = 200, y = 2)
+
+Label(win, text = "Nhập file \nâm thanh", font = ('Times new roman', 13), bg = 'white', fg = 'Black', width = 13, height = 2, relief='solid', borderwidth = 2).place(x = 140, y = 60)
+input = Text(win,font = ('Times New Roman', 13), height = 2, width = 16, relief='solid', borderwidth = 2)
+input.configure(fg = 'black', bg = "white")
+input.place(x = 320, y = 60)
+
+Label(win, text = "Kết quả \nnhận dạng", font = ('Times new roman', 13), bg = 'orange', fg = 'Black', width = 11, height = 2, relief='solid', borderwidth = 2).place(x = 240, y = 150)
+output = Text(win,font = ('Times New Roman', 13), height = 15, width = 45, relief='solid', borderwidth = 2)
+output.configure(fg = 'black', bg = "white")
+output.place(x = 90, y = 210)
+
+Button(win, text = 'Recog', command = "", font = ('Times new roman', 15), fg = 'black', bg = 'yellow', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 160, y = 520)
+Button(win, text = 'Play', command = "", font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 260, y = 520)
+Button(win, text = 'Exit', command = exit, font = ('Times new roman', 15), fg = 'black', bg = 'brown', width = 6, height = 2, relief='solid', borderwidth = 2).place(x = 360, y = 520)
+
 
 #xu ly tep am thanh
-def convert_to_decibel(arr):
+""" def convert_to_decibel(arr):
             ref = 1
             if arr != 0:
                 return 20 * np.log10(np.abs(arr) / ref)
@@ -28,10 +53,10 @@ def data_process(filename):
         data_train.append(round(np.std(data), 2))# Do lech chuan
         data_train.append(round(np.var(data), 2))# Phuong sai
         x_input.append(data_train)
-    return x_input
+    return x_input """
 
 #mo hinh no-ron
-def hard_lim(x):
+""" def hard_lim(x):
     if x >= 0:
         return 1
     else:
@@ -188,4 +213,6 @@ if __name__ == "__main__":
             break
     print("=========*=============")
     print("--Enter: 'cnt' if you want to check and test file test\n--Enter: 'exit' if you want to get out of here")
-    Selection(file_train, x_inputs, w_weights, bias) 
+    Selection(file_train, x_inputs, w_weights, bias)  """
+
+win.mainloop()
