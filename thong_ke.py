e8a0d38c665a5ff7bb2d12ae7cee5d4f18803449
VanTho01 <144830893+VanTho01@users.noreply.github.com>
2023-10-12 19:21:33 +0700

    Hoan thien phan mem

--- thong_ke.py
+++ thong_ke.py
@@ -5,9 +5,9 @@ import numpy as np
 from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
-
-df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
-in_data = array(df.iloc[:,:])
+from docx.shared import Inches
+from tkinter import messagebox
+import os
 
 #muc 1: tong so sinh vien
 def sum_sv(in_data):
@@ -22,16 +22,41 @@ def tong_sv_type(in_data):
     for i in range(3, 11):
         tong_Ldiem.append(np.sum(in_data[:, i]))
     result = dict(zip(loai_diem, tong_Ldiem))
-    return result
+    data = result
+    categories = list(data.keys())
+    values = list(data.values())
+    plt.figure(1, figsize=(8, 8))
+    plt.bar(categories, values)
+    plt.xlabel('Loại điểm')
+    plt.ylabel('Số lượng')
+    plt.title('Biểu đồ thể hiện số lượng từng loại điểm')
+    plt.yticks(np.arange(0, 100, 5))
+    plt.savefig('muc_2.png')
+    #plt.show()
+    plt.close()
+    return ''
 
 #muc 3.1: Phần trăm số SV đạt của môn học
 def phan_tram_dat(in_data):
+    part = ['sv đạt', 'sv trượt']
+    rate = []
     sv_dat = []
     for i in range(1, 9):
         n = np.array(in_data[i, :])
         sv_dat.append(np.sum(n[3:10]))
     phan_tram_sv_dat = np.sum(sv_dat) / sum_sv(in_data)
-    return f'{round((phan_tram_sv_dat*100), 2)}%'
+    rate.append(phan_tram_sv_dat)
+    rate.append(1 - phan_tram_sv_dat)
+    colors = ['lightgreen', 'lightcoral']
+    explode = (0, 0.1)  # Để phân cách một phần nhỏ (loại Đủ) ra xa     
+    plt.figure(2, figsize=(8, 8))
+    plt.pie(rate, explode=explode, labels=part, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.title('Số lượng sinh viên đạt và không đạt\n')
+    plt.axis('equal')  # Đảm bảo biểu đồ hình tròn hoàn chỉnh
+    plt.savefig('muc_3.png')
+    #plt.show()
+    plt.close()
+    return ''
 
 #muc 3:Tìm lớp có số SV đạt >=D nhiều nhất/ ít nhất
 def Max_Min_pass_class(in_data):
@@ -47,7 +72,7 @@ def Max_Min_pass_class(in_data):
     a2 = sv_dat.index(min(sv_dat))
     lop2.append(lop1[a2])
     Dict1 = dict(zip(option, lop2))
-    return Dict1
+    return f'-- Lớp có số sinh viên đạt nhiều nhất là: {Dict1["Max"]}\n-- Lớp có số sinh viên đạt nhiều nhất là: {Dict1["Min"]}'
 
 #muc 4: lop co nhieu/it diem A, B, C nhat
 def ABC_Max_Min(in_data):
@@ -67,8 +92,9 @@ def ABC_Max_Min(in_data):
         my_dict = dict(zip(option2, lop2))
         lop3.append(my_dict)
     my_dict1 = dict(zip(option1, lop3))
-    
-    return my_dict1
+    df = pd.DataFrame.from_dict(my_dict1, orient='index')
+    return df
+    #return my_dict1
 
 #muc 5: tim diem nao co sinh vien dat nhieu nhat
 def Type_max_min(in_data):
@@ -84,7 +110,7 @@ def Type_max_min(in_data):
     ind2 = tong_Ldiem.index(min(tong_Ldiem))
     loai_diem2.append(loai_diem1[ind2])
     result = dict(zip(option, loai_diem2))
-    return result
+    return f'-- Loại điểm sinh viên đạt được nhiều nhất là: {result["Max"]}\n-- Loại điểm sinh viên đạt được ít nhất là: {result["Min"]}'
 
 #muc 6: Tìm TBC số sv đạt điểm A,B.. của cả 9 lớp
 def tbc_loai_diem(in_data):
@@ -94,6 +120,17 @@ def tbc_loai_diem(in_data):
     for i in range(3, 11):
         tbc.append(round(np.mean((in_data[:, i])), 0))
     mydict = dict(zip(loai_diem1, tbc))
+    data = mydict
+    categories = list(data.keys())
+    values = list(data.values())
+    plt.figure(3, figsize=(8, 8))
+    plt.bar(categories, values)
+    plt.xlabel('Loại điểm')
+    plt.ylabel('Số lượng sinh viên')
+    plt.title('Biểu đồ thể hiện \ntrung bình sinh viên đạt từng loại điểm của 9 lớp')
+    #plt.show()
+    plt.savefig('muc_7.png')
+    plt.close()
     return mydict
 
 #mục 7: tổng số sinh viên đạt bài cuối kỳ
@@ -104,15 +141,13 @@ def sv_ck(in_data):
 #muc 8: so sanh so sinh vien dat chuan L1, L2
 def ipL1_l2(in_data):
     sv_L1 = np.sum(in_data[:, 12])
-    print("so sinh vien dat chuan dau ra L1: ", sv_L1)
     sv_L2 = np.sum(in_data[:, 13])
-    print("so sinh vien dat chuan dau ra L2: ", sv_L2)
     if (sv_L1 > sv_L2) :
-        return "=> so sinh vien dat L1 lon hon L2"
+        return f'=> Số sinh viên đạt L1 lớn hơn L2 với L1: {sv_L1} và L2: {sv_L2}'
     elif (sv_L1 < sv_L2):
-        return "=> so sinh vien dat L2 lon hon L1"
+        return f'=> Số sinh viên đạt L2 lớn hơn L1 với L1: {sv_L1} và L2: {sv_L2}'
     else:
-        return "=> so sinh vien dat L2 bang L1"
+        return f'=> Số sinh viên đạt L2 bằng L1 với L1: {sv_L1} và L2: {sv_L2}'
 
 #muc 9: do thi pho diem tung lop
 def draw_graph_mark(in_data):
@@ -146,12 +181,15 @@ def draw_graph_mark(in_data):
     plt.yticks(np.arange(0, 65, 2))
     plt.legend()
     fig = plt.gcf()
-    fig.set_size_inches(17, 8) 
-    plt.show()
-    return 'Đã hoàn thành'
+    fig.set_size_inches(19, 8)
+    plt.savefig('muc_10.png', dpi = 300) 
+    #plt.show()
+    plt.close()
+    return ''
 
 # muc 10: số phần trăm từng loại điểm và hiển thị bằng biểu đồ hình tròn:
 def phantram_loai_diem(in_data):
+    df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
     diem_loai = df.loc[:, 'Loại A':'Loại F']
     tong_so_sv = diem_loai.sum() # tính tổng số sinh viên
     labels = tong_so_sv.index  # Tên loại điểm (A, B+, B, C+, C, D+, D, F)
@@ -159,12 +197,14 @@ def phantram_loai_diem(in_data):
     colors = ['red', 'yellowgreen', 'lightblue', 'darkgreen', 'blue', 'pink', 'lightsalmon', 'brown']
     explode = (0.1, 0, 0, 0, 0, 0, 0, 0)  # Để phân cách một phần nhỏ (loại A) ra xa
 
-    plt.figure(figsize=(8, 8))
+    plt.figure(4, figsize=(8, 8))
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-    plt.title('Phan phoi diem theo loai')
+    plt.title('Phân phối điểm theo loại')
     plt.axis('equal')  # Đảm bảo biểu đồ hình tròn hoàn chỉnh
-    plt.show()
-    return 'Đã hoàn thành'
+    plt.savefig('muc_11.png')
+    #plt.show()
+    plt.close()
+    return ''
 # muc 11: Tính số phần trăm sinh viên đi thi đầy đủ
 def phantram_sv_tham_gia_day_du(in_data):
     so_sv_du = np.sum(in_data[:, 15])
@@ -175,61 +215,105 @@ def phantram_sv_tham_gia_day_du(in_data):
     sizes = [so_sv_du, so_sv_khong_du]
     colors = ['lightgreen', 'lightcoral']
     explode = (0.1, 0)  # Để phân cách một phần nhỏ (loại Đủ) ra xa     
-    plt.figure(figsize=(8, 8))
+    plt.figure(5, figsize=(8, 8))
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
-    plt.title('Phân phối số lượng sinh viên tham gia thi KTHP')
+    plt.title('Phân phối số lượng sinh viên tham gia thi KTHP\n')
     plt.axis('equal')  # Đảm bảo biểu đồ hình tròn hoàn chỉnh
-    plt.show()
-    return 'Đã hoàn thành'
+    plt.savefig('muc_12.png')
+    #plt.show()
+    plt.close()
+    return ''
 
 #muc 13: xuất báo cáo theo từng mục
-def xuat_report(in_data):
+def xuat_report(in_data, teacher_name, subject, faculty, filesave):
+    try:
+        doc = Document()
 
-    doc = Document()
+        #tiêu đề
+        Title = 'BÁO CÁO KẾT QUẢ KẾT THÚC HỌC PHẦN'
+        title = doc.add_paragraph(Title)
+        title.alignment = WD_ALIGN_PARAGRAPH.CENTER  # Căn giữa tiêu đề
+        
+        doc.add_paragraph('\n')
 
-    title = doc.add_heading('BÁO CÁO KẾT QUẢ KẾT THÚC HỌC PHẦN ', 0)
-    title.alignment = WD_ALIGN_PARAGRAPH.CENTER  # Căn giữa tiêu đề
+        # Thông tin hoc phan
 
-    # Thông tin học sinh
-    teacher_name = 'Ths. Phạm Thị Quỳnh Trang'
-    subject = 'Lập trình Python'
-    faculty = 'Khoa Điện Tử'
+        Giang_vien = f'Giảng viên: {teacher_name}'
+        Hoc_phan = f'Học phần: {subject}'
+        Khoa = f'Khoa: {faculty}'
 
-    # Thêm thông tin học sinh vào tài liệu
-    doc.add_paragraph(f'Giảng viên: {teacher_name}', style='Heading 1')
-    doc.add_paragraph(f'Học phần: {subject}', style='Heading 2')
-    doc.add_paragraph(f'Khoa: {faculty}', style='Heading 2')
+        Text1 = [Giang_vien, Hoc_phan, Khoa]
+        for txt in Text1:
+            title = doc.add_paragraph(txt)
+            title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
 
-    # Thêm kết quả học tập
-    doc.add_heading('Kết quả học tập', level=1)
+        doc.add_paragraph('\n')
 
-    # Thêm bảng điểm
-    table = doc.add_table(rows=1, cols=3)
-    table.autofit = True
-    table.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        # Thêm kết quả học tập
+        title = doc.add_paragraph('A. Thống kê kết quả kết thúc học phần')
+        title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
+        Text3 = []
+        muc_1 = f'=>Mục 1: Tổng số SV tham gia môn học là: {sum_sv(in_data)} sinh viên'
+        Text3.append(muc_1)
+        muc_2 = f'=>Mục 2: Tổng số lượng điểm từng loại : \n{tong_sv_type(in_data)}'
+        Text3.append(muc_2)
+        muc_3 = f'=>Mục 3: Phần trăm số sinh viên đạt là: \n{phan_tram_dat(in_data)}'
+        Text3.append(muc_3)
+        muc_4 = f'=>Mục 4: Lớp có số sinh viên đạt nhiều/ít nhất: \n{Max_Min_pass_class(in_data)}'
+        Text3.append(muc_4)
+        muc_5 = f'=>Mục 5: Lớp có sinh viên nhiều/ít nhất ở từng loại điểm: \n{ABC_Max_Min(in_data)}'
+        Text3.append(muc_5)
+        muc_6 = f'=>Mục 6: Loại điểm có sinh viên đạt nhiều/ ít nhất: \n{Type_max_min(in_data)}'
+        Text3.append(muc_6)
+        muc_7 = f'=>Mục 7: Trung bình sinh viên đạt từng loại điểm của từng lớp: \n{tbc_loai_diem(in_data)}'
+        Text3.append(muc_7)
+        muc_8 = f'=>Mục 8: Tổng số sinh viên đạt bài cuối kỳ: \n--Số sinh viên đạt bài cuối kỳ là: {sv_ck(in_data)}'
+        Text3.append(muc_8)
+        muc_9 = f'=>Mục 9: So sánh tổng số sinh viên đạt chuẩn đầu ra L1 so với L2: \n{ipL1_l2(in_data)}'
+        Text3.append(muc_9)
+        muc_10 = f'=>Mục 10: Đồ thị phổ điểm của từng lớp: \n{draw_graph_mark(in_data)}'
+        Text3.append(muc_10)
+        muc_11 = f'=>Mục 11: Tỉ lệ phần trăm từng loại điểm: \n{phantram_loai_diem(in_data)}'
+        Text3.append(muc_11)
+        muc_12 = f'=>Mục 12: Tỉ lệ phần trăm số sinh viên đi thi đầy đủ: \n{phantram_sv_tham_gia_day_du(in_data)}'
+        Text3.append(muc_12)
+        
+        for txt, i in zip(Text3, range(1, len(Text3)+1)):
+            if (txt == muc_2 or txt == muc_7):
+                title = doc.add_paragraph(txt)
+                image_path = f'muc_{i}.png'  # Thay thế bằng đường dẫn đến tệp ảnh thực tế
+                run = title.add_run()
+                run.add_picture(image_path, width=Inches(6), height=Inches(5))
+                title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
+                os.remove(f'muc_{i}.png')
+            elif (txt == muc_10):
+                title = doc.add_paragraph(txt)
+                image_path = f'muc_{i}.png'  # Thay thế bằng đường dẫn đến tệp ảnh thực tế
+                run = title.add_run()
+                picture = run.add_picture(image_path, width=Inches(8), height=Inches(5))
+                picture.rotation = 90
+                title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
+                os.remove(f'muc_{i}.png')
+            elif (txt == muc_3 or txt == muc_11 or txt == muc_12):
+                title = doc.add_paragraph(txt)
+                image_path = f'muc_{i}.png'  # Thay thế bằng đường dẫn đến tệp ảnh thực tế
+                run = title.add_run()
+                picture = run.add_picture(image_path, width=Inches(6), height=Inches(6))
+                picture.rotation = 90
+                title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
+                os.remove(f'muc_{i}.png')
+            else:
+                title = doc.add_paragraph(txt)
+                title.alignment = WD_ALIGN_PARAGRAPH.LEFT  # Căn giữa tiêu đề
+        title_style = title.style
+        title_font = title_style.font
+        title_font.name = 'Times New Roman'  # Tên font chữ
+        title_font.size = Pt(13)  # Kích thước chữ (Pt)
+        title_font.bold = True  # In đậm
+        title_font.italic = False  # Nghiêng chữ
+        # Lưu tài liệu vào tệp Word
+        doc.save(f'{filesave}.docx')
 
-    # Đặt tiêu đề cho các cột
-    table.rows[0].cells[0].text = 'Môn học'
-    table.rows[0].cells[1].text = 'Điểm'
-    table.rows[0].cells[2].text = 'Xếp loại'
-
-    # Giả định danh sách môn học, điểm, và xếp loại
-    subjects = ['Toán', 'Văn', 'Anh Văn', 'Lý', 'Hóa']
-    grades = [8.5, 7.75, 9.0, 8.0, 7.5]
-
-    # Thêm thông tin môn học, điểm, và xếp loại vào bảng
-    for subject, grade in zip(subjects, grades):
-        row = table.add_row().cells
-        row[0].text = subject
-        row[1].text = str(grade)
-        if grade >= 8.0:
-            row[2].text = 'Giỏi'
-        elif grade >= 6.5:
-            row[2].text = 'Khá'
-        else:
-            row[2].text = 'Trung bình'
-
-    # Lưu tài liệu vào tệp Word
-    doc.save('bao_cao_ket_qua_hoc_tap.docx')
-
-    print('Báo cáo đã được tạo và lưu thành công.')
+        messagebox.showinfo('Thông báo','Báo cáo đã được tạo và lưu thành công.')
+    except PermissionError:
+        messagebox.showwarning('Cảnh báo','File được lưu đang được mở.')
